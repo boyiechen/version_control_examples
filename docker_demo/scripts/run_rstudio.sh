4aaee9c7a9010e4607c0b0f58aa7ee852d1c1465
root <root@BOYIE-LT.gsb.uchicago.edu>
2023-06-22 16:15:28 -0500

    add rstudio image

--- docker_demo/scripts/run_rstudio.sh
+++ docker_demo/scripts/run_rstudio.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
+# Install renv
+R -e 'install.packages("renv")'
+
+# Update package list and install git
+apt-get update && apt-get install -y git
+apt-get install -y libgsl-dev
+
+# Clone the repository
+cd /home/rstudio/workspace
+git clone https://github.com/boyiechen/version_control_examples.git
+
+# Modify .env file to change path to mapped Dropbox path
+sed -i 's#^DROPBOX_PATH=.*#DROPBOX_PATH=/dropbox#g' version_control_examples/svar_model/.env
+
+# Run master_script.R
+cd version_control_examples/svar_model
+Rscript code/utility/requirements.R
+
+
 # run container from image
-docker run --rm -ti -e DISABLE_AUTH=true -p 8787:8787 rocker/rstudio
+# docker run --rm -ti -e DISABLE_AUTH=true -p 8787:8787 rocker/rstudio
